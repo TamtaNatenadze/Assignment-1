84342d67717851982491e55d3d52ad44a95f8574
TamtaNatenadze <tamta.natenadze.1@iliauni.edu.ge>
2024-07-25 13:48:20 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log('Name is : Tamta');
+console.log('Surname is : Natenadze');
+console.log('Age : XXX');
+console.warn('Email : XXX');
+console.warn('Passwod is : XXX');
+console.log('Phone : XXX');
